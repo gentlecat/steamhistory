1de2c5df50abc6ae2f1b611e90c6472a64a1966c
Roman Tsukanov <roman@tsukanov.me>
2014-01-11 15:54:34 +1000

    Added basicHandler into webface. Reorganized some functions to simplify readability.

--- webface/webface.go
+++ webface/webface.go
@@ -25,7 +25,21 @@ import (
 	"strconv"
 )
 
-var mc *memcache.Client = memcache.New("localhost:11211")
+// Start starts FastCGI server at 127.0.0.1:9000
+func Start() {
+	log.Println("Starting server...")
+	l, err := net.Listen("tcp", "127.0.0.1:9000")
+	if err != nil {
+		log.Fatal("Failed to start server!", err)
+	}
+	fcgi.Serve(l, makeRouter())
+}
+
+// StartDev starts a development server at localhost:8080
+func StartDev() {
+	log.Println("Starting development server (localhost:8080)...")
+	http.ListenAndServe(":8080", makeRouter())
+}
 
 func makeRouter() *mux.Router {
 	r := mux.NewRouter()
@@ -38,22 +52,34 @@ func makeRouter() *mux.Router {
 	return r
 }
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
+var mc *memcache.Client = memcache.New("localhost:11211")
+
+/*
+ * Handlers
+ */
+
+// basicHandler just loads specified template, combines it with base template
+// and writes result into ResponseWriter.
+func basicHandler(w http.ResponseWriter, r *http.Request, file string) {
 	exeloc, err := osext.ExecutableFolder()
 	t, err := template.ParseFiles(
 		exeloc+"webface/templates/base.html",
-		exeloc+"webface/templates/home.html")
+		exeloc+"webface/templates/"+file)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		log.Println(err)
 		return
 	}
-	err = t.Execute(w, r.URL)
+	err = t.Execute(w, nil)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		log.Println(err)
 		return
 	}
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	basicHandler(w, r, "home.html")
 }
 
 func historyHandler(w http.ResponseWriter, r *http.Request) {
@@ -107,21 +133,7 @@ func historyHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func topHandler(w http.ResponseWriter, r *http.Request) {
-	exeloc, err := osext.ExecutableFolder()
-	t, err := template.ParseFiles(
-		exeloc+"webface/templates/base.html",
-		exeloc+"webface/templates/top.html")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		log.Println(err)
-		return
-	}
-	err = t.Execute(w, r.URL)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		log.Println(err)
-		return
-	}
+	basicHandler(w, r, "top.html")
 }
 
 func dailyTopHandler(w http.ResponseWriter, r *http.Request) {
@@ -191,35 +203,5 @@ func searchHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
-	exeloc, err := osext.ExecutableFolder()
-	t, err := template.ParseFiles(
-		exeloc+"webface/templates/base.html",
-		exeloc+"webface/templates/about.html")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		log.Println(err)
-		return
-	}
-	err = t.Execute(w, nil)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		log.Println(err)
-		return
-	}
-}
-
-// Start starts FastCGI server at 127.0.0.1:9000
-func Start() {
-	log.Println("Starting server...")
-	l, err := net.Listen("tcp", "127.0.0.1:9000")
-	if err != nil {
-		log.Fatal("Failed to start server!", err)
-	}
-	fcgi.Serve(l, makeRouter())
-}
-
-// StartDev starts a development server at localhost:8080
-func StartDev() {
-	log.Println("Starting development server (localhost:8080)...")
-	http.ListenAndServe(":8080", makeRouter())
+	basicHandler(w, r, "about.html")
 }
