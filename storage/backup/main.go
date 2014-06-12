8fc1656c42af4a362f6cb4a17c08e4fdfbbbc544
Roman Tsukanov <roman@tsukanov.me>
2014-06-13 03:22:03 +1000

    Added support for uploading backups to Amazon Glacier.

---
+++ storage/backup/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"code.google.com/p/goconf/conf"
+	"github.com/rdwilliamson/aws"
+	"github.com/rdwilliamson/aws/glacier"
+)
+
+func main() {
+	c, err := conf.ReadConfigFile("aws.config")
+	if err != nil {
+		log.Fatal("Failed to load config file! ", err)
+	}
+
+	secret, err := c.GetString("default", "secret")
+	if err != nil {
+		log.Fatal("Failed to get AWS secret key from config! ", err)
+	}
+	access, err := c.GetString("default", "access")
+	if err != nil {
+		log.Fatal("Failed to get AWS access key from config! ", err)
+	}
+
+	conn := glacier.NewConnection(secret, access, aws.USEast)
+
+	// Opening backup file
+	name, err := c.GetString("glacier", "backup-file")
+	if err != nil {
+		log.Fatal(err)
+	}
+	archive, err := os.Open(name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer archive.Close()
+
+	vault, err := c.GetString("glacier", "vault")
+	if err != nil {
+		log.Fatal(err)
+	}
+	id, err := conn.UploadArchive(vault, archive, "backup")
+	if err != nil {
+		log.Fatal("Failed to upload archive! ", err)
+	}
+
+	fmt.Printf("Archive uploaded. ID: %s", id)
+}
